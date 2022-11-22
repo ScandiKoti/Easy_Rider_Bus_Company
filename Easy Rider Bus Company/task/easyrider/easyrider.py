8816c71b8295467910eb0c7121073024b360bf2d
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 05:21:58 +0300

    Stage 5/6: Unlost in time

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -3,20 +3,18 @@ import json
 
 def main():
     input_data = json.loads(input())
-    for bus_id in set(rec['bus_id'] for rec in input_data):
-        if sum(rec['stop_type'] == 'S' for rec in input_data if rec['bus_id'] == bus_id) != 1 \
-                or sum(rec['stop_type'] == 'F' for rec in input_data if rec['bus_id'] == bus_id) != 1:
-            print(f'There is no start or end stop for the line: {bus_id}')
-            exit()
-
-    start_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'S')
-    print(f'Start stops: {len(start_stops)} {sorted(list(start_stops))}')
-
-    transfer_stops = set(trans['stop_name'] for trans in input_data if sum(rec['stop_name'] == trans['stop_name'] for rec in input_data) > 1)
-    print(f'Transfer stops: {len(transfer_stops)} {sorted(list(transfer_stops))}')
-
-    finish_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'F')
-    print(f'Finish stops: {len(finish_stops)} {sorted(list(finish_stops))}')
+    print("Arrival time test:")
+    errors = []
+    last_id = ''
+    last_time = "00:00"
+    output = ""
+    for record in input_data:
+        if not record["bus_id"] in errors and last_id == record["bus_id"] and last_time >= record["a_time"]:
+            output += "bus_id line {}: wrong time on station {}\n".format(str(record["bus_id"]), record["stop_name"])
+            errors.append(record["bus_id"])
+        last_id = record["bus_id"]
+        last_time = record["a_time"]
+    print(output if output else "OK")
 
 
 if __name__ == "__main__":
