5d423837d18afe1f0b715055e676bd8844ff9420
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 05:21:32 +0300

    Stage 4/6: Special stops

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -2,15 +2,21 @@ import json
 
 
 def main():
-    check_dict = json.loads(input())
-    bus_id_dict = dict()
-    for catalog in check_dict:
-        if catalog['bus_id'] not in bus_id_dict:
-            bus_id_dict[catalog['bus_id']] = 0
-        bus_id_dict[catalog['bus_id']] += 1
-    print('Line names and number of stops:')
-    for key, value in bus_id_dict.items():
-        print(f'bus_id: {key}, stops: {value}')
+    input_data = json.loads(input())
+    for bus_id in set(rec['bus_id'] for rec in input_data):
+        if sum(rec['stop_type'] == 'S' for rec in input_data if rec['bus_id'] == bus_id) != 1 \
+                or sum(rec['stop_type'] == 'F' for rec in input_data if rec['bus_id'] == bus_id) != 1:
+            print(f'There is no start or end stop for the line: {bus_id}')
+            exit()
+
+    start_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'S')
+    print(f'Start stops: {len(start_stops)} {sorted(list(start_stops))}')
+
+    transfer_stops = set(trans['stop_name'] for trans in input_data if sum(rec['stop_name'] == trans['stop_name'] for rec in input_data) > 1)
+    print(f'Transfer stops: {len(transfer_stops)} {sorted(list(transfer_stops))}')
+
+    finish_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'F')
+    print(f'Finish stops: {len(finish_stops)} {sorted(list(finish_stops))}')
 
 
 if __name__ == "__main__":
