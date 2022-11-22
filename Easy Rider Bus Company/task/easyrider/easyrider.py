a846ce472b0b74ae2e863b997b312c13214dd746
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 05:12:24 +0300

    Stage 3/6: Bus line info

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,31 +1,11 @@
 import json
-import re
+from collections import Counter
 
 
 def main():
-    check_dict = json.loads(input())
-    err = {key: 0 for key in check_dict[0].keys()}
-    for catalog in check_dict:
-        for key, value in catalog.items():
-            if key == 'stop_name':
-                if not isinstance(value, str) or len(value) < 1:
-                    err[key] += 1
-                elif not re.search(r'([A-Z][a-z]+ )+(Street|Avenue|Boulevard|Road)$', str(value)):
-                    err[key] += 1
-            elif key == 'stop_type':
-                if not isinstance(value, str) or len(value) > 1:
-                    err[key] += 1
-                elif not re.match('[SOF]?$', str(value)):
-                    err[key] += 1
-            elif key == 'a_time':
-                if not isinstance(value, str) or len(value) < 5:
-                    err[key] += 1
-                elif not re.match(r'(2[0-3]|[01]\d):[0-5]\d$', str(value)):
-                    err[key] += 1
-    print(f'Format validation: {sum(err.values())} errors')
-    for key, value in err.items():
-        if key in ('stop_name', 'stop_type', 'a_time'):
-            print(f'{key}: {value}')
+    bus_id_counter = Counter([value for el in json.loads(input()) for key, value in el.items() if key == 'bus_id'])
+    print('Line names and number of stops:')
+    print('\n'.join(f'bus_id: {key}, stops: {value}' for key, value in bus_id_counter.items()))
 
 
 if __name__ == "__main__":
