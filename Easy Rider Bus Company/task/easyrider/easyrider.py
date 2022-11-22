9eb97e8d31f977d157f0ee6b6e595895d2f58065
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 06:01:43 +0300

    Stage 6/6: On-demand

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -3,18 +3,15 @@ import json
 
 def main():
     input_data = json.loads(input())
-    print("Arrival time test:")
-    errors = []
-    last_id = ''
-    last_time = "00:00"
-    output = ""
-    for record in input_data:
-        if not record["bus_id"] in errors and last_id == record["bus_id"] and last_time >= record["a_time"]:
-            output += "bus_id line {}: wrong time on station {}\n".format(str(record["bus_id"]), record["stop_name"])
-            errors.append(record["bus_id"])
-        last_id = record["bus_id"]
-        last_time = record["a_time"]
-    print(output if output else "OK")
+    start_stops = set(record['stop_name'] for record in input_data if record['stop_type'] == 'S')
+    transfer_stops = set(i['stop_name'] for i in input_data if sum(record['stop_name'] == i['stop_name'] for record
+                                                                   in input_data) > 1)
+    finish_stops = set(record['stop_name'] for record in input_data if record['stop_type'] == 'F')
+    union_stops = start_stops | transfer_stops | finish_stops
+    error_stops = set(record['stop_name'] for record in input_data if record['stop_type'] == 'O' and record['stop_name']
+                      in union_stops)
+    print('On demand stops test:')
+    print(f'Wrong stop type: {list(sorted(error_stops)) if error_stops else "OK"}')
 
 
 if __name__ == "__main__":
