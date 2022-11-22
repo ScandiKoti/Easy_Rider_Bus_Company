e9af630847035a604acd62377f5d675e55251335
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 05:05:48 +0300

    Stage 1/6: Checking the data type

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -2,21 +2,24 @@ import json
 
 
 def main():
-    input_data = json.loads(input())
-    for bus_id in set(rec['bus_id'] for rec in input_data):
-        if sum(rec['stop_type'] == 'S' for rec in input_data if rec['bus_id'] == bus_id) != 1 \
-                or sum(rec['stop_type'] == 'F' for rec in input_data if rec['bus_id'] == bus_id) != 1:
-            print(f'There is no start or end stop for the line: {bus_id}')
-            exit()
-
-    start_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'S')
-    print(f'Start stops: {len(start_stops)} {sorted(list(start_stops))}')
-
-    transfer_stops = set(trans['stop_name'] for trans in input_data if sum(rec['stop_name'] == trans['stop_name'] for rec in input_data) > 1)
-    print(f'Transfer stops: {len(transfer_stops)} {sorted(list(transfer_stops))}')
-
-    finish_stops = set(rec['stop_name'] for rec in input_data if rec['stop_type'] == 'F')
-    print(f'Finish stops: {len(finish_stops)} {sorted(list(finish_stops))}')
+    check_dict = json.loads(input())
+    err = {key: 0 for key in check_dict[0].keys()}
+    for catalog in check_dict:
+        for key, value in catalog.items():
+            if key in ('bus_id', 'stop_id', 'next_stop'):
+                if not isinstance(value, int):
+                    err[key] += 1
+            elif key in ('stop_name', 'a_time'):
+                if not isinstance(value, str) or len(value) < 1:
+                    err[key] += 1
+            elif key == 'stop_type':
+                if not isinstance(value, str) or len(value) > 1:
+                    err[key] += 1
+                elif isinstance(value, str) and value not in 'SOF':
+                    err[key] += 1
+    print(f'Type and required field validation: {sum(err.values())} errors')
+    for key, value in err.items():
+        print(f'{key}: {value}')
 
 
 if __name__ == "__main__":
