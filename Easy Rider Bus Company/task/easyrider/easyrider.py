3b566ac0f8ce728c8ab3c33c45495e7cd47a331c
Nikolay Anisimov <109425593+SkandiKoti@users.noreply.github.com>
2022-11-22 05:17:08 +0300

    Stage 3/6: Bus line info

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,11 +1,16 @@
 import json
-from collections import Counter
 
 
 def main():
-    bus_id_counter = Counter([value for el in json.loads(input()) for key, value in el.items() if key == 'bus_id'])
+    check_dict = json.loads(input())
+    bus_id_dict = dict()
+    for catalog in check_dict:
+        if catalog['bus_id'] not in bus_id_dict:
+            bus_id_dict[catalog['bus_id']] = 0
+        bus_id_dict[catalog['bus_id']] += 1
     print('Line names and number of stops:')
-    print('\n'.join(f'bus_id: {key}, stops: {value}' for key, value in bus_id_counter.items()))
+    for key, value in bus_id_dict.items():
+        print(f'bus_id: {key}, stops: {value}')
 
 
 if __name__ == "__main__":
